e596085bea818c028b00f2c3f83282af62110854
vincelin <904481710@qq.com>
2020-12-02 15:43:49 +0800

    init Swagger

---
+++ src/main/java/com/robots/bumblebee/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package com.robots.bumblebee.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Value("${jwt.token-start-with}")
+    private String tokenStartWith;
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     *
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        ticketPar.name(tokenHeader).description("token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .defaultValue(tokenStartWith + " ")
+                .required(true)
+                .build();
+        pars.add(ticketPar.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.robots.bumblebee.login"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("开放平台.开发文档")
+//                .description("开发文档")
+                .version("0.0.1")
+                .build();
+    }
+}
--- src/main/java/com/robots/bumblebee/login/LoginController.java
+++ src/main/java/com/robots/bumblebee/login/LoginController.java
@@ -1,22 +1,58 @@
 package com.robots.bumblebee.login;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "简书-演示",description = "用来演示Swagger的一些注解")
+
 public class LoginController{
     private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     private JWTService jwtService;
 
-    @RequestMapping("/login")
-    public ResponseEntity<Object> login(String user, String pwd){
+    @ApiOperation(value="登录", notes="登录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", name = "user", value = "用户ID", required = true, dataType = "String"),
+            @ApiImplicitParam(paramType="query", name = "pwd", value = "密码", required = true, dataType = "String")
+    })
+    @GetMapping("/login")
+    public ResponseEntity<Object> login(@RequestParam(value="user") String user, @RequestParam(value="pwd") String pwd){
         String token = jwtService.createToken(user);
 
         return ResponseEntity.ok(token);
+    }
+
+    @ApiOperation(value="修改用户密码", notes="根据用户id修改密码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", name = "userId", value = "用户ID", required = true, dataType = "Integer"),
+            @ApiImplicitParam(paramType="query", name = "password", value = "旧密码", required = true, dataType = "String"),
+            @ApiImplicitParam(paramType="query", name = "newPassword", value = "新密码", required = true, dataType = "String")
+    })
+
+    @GetMapping("/updatePassword")
+    public String updatePassword(@RequestParam(value="userId") Integer userId, @RequestParam(value="password") String password,
+                                 @RequestParam(value="newPassword") String newPassword){
+        if(userId <= 0 || userId > 2){
+            return "未知的用户";
+        }
+        if(StringUtils.isEmpty(password) || StringUtils.isEmpty(newPassword)){
+            return "密码不能为空";
+        }
+        if(password.equals(newPassword)){
+            return "新旧密码不能相同";
+        }
+        return "密码修改成功!";
     }
 }
